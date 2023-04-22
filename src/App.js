1888dc7e5e82e0218424279386b0982144e6a3b4
YazanRihan <yazanrihan@gmail.com>
2023-04-23 02:58:04 +0400

    Added Slogan and image to home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import pills from './images/pills.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <dev class="dividor">
+      <dev class="sloganC"> 
+        <h1 class="slogan">Medicine Is Not A Book But Mind, Not A Business But Life</h1> 
+        <button> Donate</button>
+      </dev>
+      <dev class="pillsC">
+        <img class="pills" src={pills} alt='Pills'></img>
+      </dev>
+    </dev>;
+      
+  }
 }
 
 export default App;
-// ismail
