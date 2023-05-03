9a3b30bd90131d133fc7932ed9822bb2c2d9d675
YazanRihan <yazanrihan@gmail.com>
2023-05-03 17:15:20 +0400

    Finish making form

--- src/Donate.js
+++ src/Donate.js
@@ -1,6 +1,6 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import './Donate.css'; // Import the CSS file you'll create later
-
+import Button from '@mui/material/Button';
 
 class DonateForm extends Component {
 
@@ -17,6 +17,15 @@ class DonateForm extends Component {
     }
 
     handleMedicineTypeChange = (event) => {
+        const Lotions = document.getElementById('Lotions')
+        const Drops = document.getElementById('Drops')
+        const Syrups = document.getElementById('Syrups')
+        const Pills = document.getElementById('Pills')
+        Lotions.style.backgroundColor = "#cccccc";
+        Drops.style.backgroundColor = "#cccccc";
+        Syrups.style.backgroundColor = "#cccccc";
+        Pills.style.backgroundColor = "#cccccc";
+        event.target.style.backgroundColor = '#57b9da';
         this.setState({medicineType: event.target.id});
     }
     handleQuantityChange = (event) => {
@@ -31,54 +40,70 @@ class DonateForm extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
         const {medicineType, quantity, expiry, name} = this.state;
-        if (medicineType === '' || quantity === '' || expiry === '' || name === '')
-        console.log('Medicine Type:', medicineType);
-        console.log('quantity:', quantity);
-        console.log('Expiry:', expiry);
-        console.log('name:', name);
 
-        this.setState({medicineType: '',
-        quantity: '',
-        expiry: '',
-        name: ''});
+        if (medicineType === '' || quantity === '' || expiry === '' || name === '') {
+            window.alert("Please fill all the fields");
+        }
+        else {
+
+            console.log('Medicine Type:', medicineType);
+            console.log('quantity:', quantity);
+            console.log('Expiry:', expiry);
+            console.log('name:', name);
+            
+            const Lotions = document.getElementById('Lotions')
+            const Drops = document.getElementById('Drops')
+            const Syrups = document.getElementById('Syrups')
+            const Pills = document.getElementById('Pills')
+            Lotions.style.backgroundColor = "#cccccc";
+            Drops.style.backgroundColor = "#cccccc";
+            Syrups.style.backgroundColor = "#cccccc";
+            Pills.style.backgroundColor = "#cccccc";
+            
+            this.setState({medicineType: '',
+            quantity: '',
+            expiry: '',
+            name: ''});
+        }
     }
 
     render() {
-        const {medicineType, quantity, expiry, name} = this.state;
+        const {quantity, expiry, name} = this.state;
         
         return (
-            <div>
+            <div class="formDiv">
 
-                <h1>Donate your Medicines</h1>
+                <h1 class="subtitle">Make your requests</h1>
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        Select medicine type
-                        <div>
-                            <div id="Lotions" onClick={this.handleMedicineTypeChange}>Lotions</div>
-                            <div id="Drops" onClick={this.handleMedicineTypeChange}>Drops</div>
-                            <div id="Syrups" onClick={this.handleMedicineTypeChange}>Syrups</div>
-                            <div id="Pills" onClick={this.handleMedicineTypeChange}>Pills</div>
+                        <h2 class='formLabel'>Select medicine type:</h2>
+                        <div class="medicineTypesContainer">
+                            <div class="typeBox" id="Lotions" onClick={this.handleMedicineTypeChange}>Lotions</div>
+                            <div class="typeBox" id="Drops" onClick={this.handleMedicineTypeChange}>Drops</div>
+                            <div class="typeBox" id="Syrups" onClick={this.handleMedicineTypeChange}>Syrups</div>
+                            <div class="typeBox" id="Pills" onClick={this.handleMedicineTypeChange}>Pills</div>
                         </div>
                     </label>
 
                     <label>
-                        Quantity
-                        <input type="number" value={quantity} onChange={this.handleQuantityChange}></input>
+                    <h2 class='formLabel'>Quantity:</h2>
+                        
+                        <input placeholder="Enter the quantity with the unit" type="text" value={quantity} onChange={this.handleQuantityChange}></input>
                     </label>
 
                     <label>
-                       Expiry date
-
-                       <input type='string' value={expiry} onChange={this.handleExpiryChange}></input>
+                    <h2 class='formLabel'>Expiry Date:</h2>
+                       
+                       <input class="dateInput" type='date' value={expiry} onChange={this.handleExpiryChange}></input>
                     </label>
 
                     <label>
-                        Name of medicine
-                        <input type='string' value={name} onChange={this.handleNameChange}></input>
-
+                    <h2 class='formLabel'>Name of Medicine</h2>
+                        
+                        <input placeholder='Enter the name of the medicine' type='string' value={name} onChange={this.handleNameChange}></input>
                     </label>
-
-                    <div onClick={this.handleSubmit}>Submit</div>
+                    <br/>
+                    <Button onClick={this.handleSubmit} variant='contained' color='secondary' size='large' style={{width: '85px', height: '40px', color: '#ffffff', fontSize:'20px', marginTop:'20px'}}>Submit</Button>
 
                 </form>
 
