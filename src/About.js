a4fafd2ab03277f0bc086fcf8d1ea296435f6364
IsmailZein <ismailzein2002@gmail.com>
2023-05-03 15:41:34 +0400

    Fixed the About.css
    Ismail

--- src/About.js
+++ src/About.js
@@ -5,25 +5,25 @@ import './About.css';
 class About extends React.Component {
   render() {
     return (
-      <div className="about">
+      <div class="about">
         <h1>About Us</h1>  
       
-        <section className="about-section">
+        <div class="Mission">
           <h2>Mission</h2>
           <p>
             To provide a reliable and accessible platform for individuals, healthcare facilities, and organizations
              to donate unused and unexpired medications to those in need, thereby reducing medication waste.
           </p>
-        </section>
+        </div>
 
-        <section className="about-section">
+        <div className="Vision">
           <h2>Vision</h2>
           <p>
             Our vision is a world where every person has access to affordable and safe medications,
              and where medication waste is minimized through effective redistribution
           </p>
-        </section>
-    <section className="about-section">
+        </div>
+    <div class="Values">
           <h2>Values</h2>
           <ol>
           <li>Accessibility: We believe that everyone should have access to essential medications, regardless of their socioeconomic status or geographic location</li>
@@ -32,7 +32,7 @@ class About extends React.Component {
           </li>
           <li>Innovation: We embrace new technologies and approaches to maximize the impact of our mission and continually improve our services.</li>
           </ol>
-        </section>
+      </div>
       </div>
   )}
 }
