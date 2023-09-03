75fad20630ffdb2ecffed535ae8f967097900f0e
IsmailZein <ismailzein2002@gmail.com>
2023-09-03 16:24:34 +0400

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -8,20 +8,19 @@ import {Link} from 'react-router-dom';  // Importing the Link component from the
 class Homepage extends React.Component {  // Creating a new class component named Homepage that extends the React.Component class
     render() {  // Defining a render method for the Homepage component
         return (  // Return a JSX template that renders HTML elements and React components
-            <div>  // A div element that wraps the JSX template
-                <div class="dividor">  // A div element with class "dividor"
-                    <div class="sloganC">  // A div element with class "sloganC"
-                        <h1 class="slogan">Medicine Is Not A Book But Mind, Not A Business But Life.</h1>  // A header element with class "slogan" and a slogan text
-                        <Button variant='contained' color='secondary' size='large' style={{width: '200px', height: '100px', color: '#ffffff', fontSize:'36px', marginTop:'100px'}}>  // A button element with variant, color, size, and style props
-                            <Link to="/Donate" class="DonateLink">Donate</Link>  // A link element with to and class props that takes the user to the Donate page
-                        </Button>  // Closing button tag
-                    </div>  // Closing div tag
-                    <div class="pillsC">  // A div element with class "pillsC"
-                        <img class="pills" src={pills} alt='Pills'></img>  // An img element with class "pills" that displays the pills image
-                    </div>  // Closing div tag
-                </div>  // Closing div tag
-                <div>  // A div element with no content
-                </div>  // Closing div tag
+            <div>  
+                <div class="dividor">  
+                    <div class="sloganC">  
+                        <h1 class="slogan">.</h1> 
+                        <Button variant='contained' color='secondary' size='large' style={{width: '200px', height: '100px', color: '#ffffff', fontSize:'36px', marginTop:'100px'}}>  
+                        Donate </Button> 
+                    </div>  
+                    <div class="pillsC">  
+                        <img class="pills" src={pills} alt='Pills'></img>   
+                    </div>  
+                </div>  
+                <div>  
+                </div>  
             </div>  // Closing div tag
         )
     }  // Closing render method
